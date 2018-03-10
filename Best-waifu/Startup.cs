1b2b673a50ddc1e2a57e80c09b79df85dbac9fd4
Voidstorm <sasha.yaroshepta@gmail.com>
2018-03-10 15:57:55 +0200

    Add project files.

---
+++ Best-waifu/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bestwaifu.Startup))]
+namespace Bestwaifu
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
