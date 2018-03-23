4ade9c59eb04cee52b886d5dc078c6692cc2e4e8
Voidstorm <sasha.yaroshepta@gmail.com>
2018-03-23 22:33:43 +0200

    Add properties to franchise fields

--- Best-waifu.Data/Entities/Franchise.cs
+++ Best-waifu.Data/Entities/Franchise.cs
@@ -1,12 +1,22 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BestWaifu.Data.Entities
 {
     public class Franchise
     {
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = @"Please input title")]
+        [StringLength(256, ErrorMessage = @"Title must be less than 100 characters")]
         public string Title { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = @"Please input author")]
+        [StringLength(100, ErrorMessage = @"Author's name must be less than 100 characters")]
+        public string Author { get; set; }
 
         public virtual ICollection<Character> Characters { get; set; }
     }
